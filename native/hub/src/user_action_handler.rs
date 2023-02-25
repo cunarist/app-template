ef9b4c9079b8446930d8e121fb178881f6660dcc
Kim Dong-Hyun <temeddix@gmail.com>
2023-02-25 14:52:51 +0900

    Update user_action_handler.rs

--- native/hub/src/user_action_handler.rs
+++ native/hub/src/user_action_handler.rs
@@ -4,9 +4,9 @@ pub async fn handle_user_action(task_address: String, json_string: String) {
     // `task_address` would be something like "some.task.address"
     // `json_string` would be something like "{'some':'json','string':true}"
     let json_value = serde_json::from_str(&json_string).unwrap();
-    let layered: Vec<&str> = task_address.split(".").collect();
+    let layered: Vec<&str> = task_address.split('.').collect();
 
-    if layered.len() == 0 {
+    if layered.is_empty() {
     } else if layered[0] == "someTaskCategory" {
         if layered.len() == 1 {
         } else if layered[1] == "calculateSomething" {
